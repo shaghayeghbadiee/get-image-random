9f31f3adb394468e14edf8cfd51a20f8e0aebbc2
Shaghayegh Badiee <78657055+shaghayeghbadiee@users.noreply.github.com>
2023-01-03 04:05:33 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+   const [img,setImg]=useState([])
+  useEffect(()=>{
+     fetch('https://random-d.uk/api/random ')
+    .then(response => response.json())
+    .then (data => {
+      console.log(data)
+      setImg(data)
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+                      
+                      <img src={img.url} alt="bd.uk" />     
     </div>
   );
 }
